888e78493fbc6d6a8a62d7ec3fa5a10d5396afe3
AlexandraEL9 Lee <alexandraelee9@gmail.com>
2024-04-29 14:32:41 +0000

    moved docs

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    //set up the grid in html
+    for (let i = 0; i < 9; i++) { //i goes from 0 to 8, stops at 9
+        //<div id="0-8"></div>
+        let tile = document.createElement("div");
+        tile.id = i.toString();
+        tile.addEventListener("click", selectTile);
+        document.getElementById("board").appendChild(tile);
+    }
+    setInterval(setMole, 1000); // 1000 miliseconds = 1 second, every 1 second call setMole
+    setInterval(setPlant, 2000); // 2000 miliseconds = 2 seconds, every 2 second call setPlant
+}
