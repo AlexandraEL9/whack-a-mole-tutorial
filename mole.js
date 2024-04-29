5e089fa06cc57f718459ec81f3c365c71c81ec43
AlexandraEL9 Lee <alexandraelee9@gmail.com>
2024-04-29 17:21:53 +0000

    add random mole and plant functionality

--- mole.js
+++ mole.js
@@ -1,5 +1,6 @@
 //3create variables
 let currMoleTile;
+let currPlantTile;
 //1
 window.onload = function() {
     setGame();
@@ -18,12 +19,27 @@ function getRandomTile() {
     return num.toString();
 }
  
-//4create function called setMole
 function setMole() {
+
+    if (currMoleTile) {
+        currMoleTile.innerHTML = "";
+    }
+
     let mole = document.createElement("img");
     mole.src = "./monty-mole.png";
     //randomly place the mole on 1 of the tiles
     let num = getRandomTile();
     currMoleTile = document.getElementById(num);
     currMoleTile.appendChild(mole);
+}
+function setPlant() {
+    if (currPlantTile) {
+        currPlantTile.innerHTML = "";
+    }
+    let plant = document.createElement("img");
+    plant.src = "./piranha-plant.png";
+
+    let num = getRandomTile();
+    currPlantTile = document.getElementById(num);
+    currPlantTile.appendChild(plant);
 }
